af0da73747fd4a9a1ef58ba54ab556384776fb53
Christopher Wiles <chriswiles87@gmail.com>
2016-09-03 15:50:31 -0500

    create GameLobby

---
+++ client/components/GameLobby.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class GameLobby extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
