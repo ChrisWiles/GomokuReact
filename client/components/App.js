885130c7fafffebcc42f0e3ad7c760b2ba84cc53
Christopher Wiles <chriswiles87@gmail.com>
2016-09-03 14:17:48 -0500

    setup boilerplate

---
+++ client/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>REACT</h1>
+      </div>
+    )
+  }
+}
---
+++ client/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+
+// Render App to DOM
+ReactDOM.render(<App />, document.getElementById('mount'))
