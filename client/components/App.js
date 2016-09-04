88f037e3380a86db05e3a5155b2215d3d6152d14
Christopher Wiles <chriswiles87@gmail.com>
2016-09-04 12:32:10 -0500

    remove subTitle

--- client/components/App.js
+++ client/components/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import GameBoard from './GameBoard'
+import GameLobby from './GameLobby'
 
 export default class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      isLobby: true
+    }
   }
 
   render() {
@@ -13,7 +16,7 @@ export default class App extends Component {
           <div className="row">
               <div className="col-md-offset-2 col-md-8">
                 <div className='title'>Gomoku</div>
-                <div className='subTitle'>Tic-Tac-Toe get 5 in a row</div>
+                {/* <div className='subTitle'>Tic-Tac-Toe get 5 in a row</div> */}
                 <div className="gameBoard"><GameBoard/></div>
               </div>
           </div>
