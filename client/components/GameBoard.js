7d4b72ec20e8763889d4006c3df17db78b81cbab
Christopher Wiles <chriswiles87@gmail.com>
2016-09-04 12:30:04 -0500

    gamelobby logic

--- client/components/GameBoard.js
+++ client/components/GameBoard.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import GameTile from './GameTile'
+import { emitBoard } from '../models/game'
 
 export default class GameBoard extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      board: []
+      board: [],
+      isTurn: true
     }
   }
 
@@ -14,9 +16,8 @@ export default class GameBoard extends Component {
   }
 
   _initializeBoard() {
-    // create 2d game board
     const board = []
-    const rows = 'abcdefghijklmno'.split('')
+    const rows = 'ABCDEFGHIJKLMNO'.split('')
     const cols = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 
     rows.forEach(row => {
@@ -30,11 +31,20 @@ export default class GameBoard extends Component {
 
   _createTiles(tiles) {
     return tiles.map((tile, i) => {
-      return <GameTile player={tile.player} id={tile.id} key={i} updatePlayerMove={this._updatePlayerMove.bind(this)}/>
+      return (
+        <GameTile
+          isTurn={this.state.isTurn}
+          player={tile.player}
+          id={tile.id}
+          key={i}
+          updatePlayerMove={this._updatePlayerMove.bind(this)}
+        />
+      )
     })
   }
 
   _updatePlayerMove(id) {
+    //this.setState({isTurn: false})
     console.log(id)
     const board = this.state.board
     board.forEach(tile => {
@@ -42,7 +52,22 @@ export default class GameBoard extends Component {
         tile.player = 1
       }
     })
+    this._isWin(board)
+  }
+
+  _isWin(board) {
+    function horizontal(b) {
+
+    }
+    function vertical(b) {
+
+    }
+    function diagonal(b) {
+
+    }
+
     this.setState({board})
+    //emitBoard(board)
   }
 
   render() {
--- client/components/GameLobby.js
+++ client/components/GameLobby.js
@@ -3,12 +3,71 @@ import React, { Component } from 'react'
 export default class GameLobby extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      inLobby: true,
+      join: false,
+      create: false,
+      code: ''
+    }
+  }
+
+  _handleChange(event) {
+    this.setState({code: event.target.value})
+  }
+
+  _genAccessCode() {
+    const char = 'abcdefghijklmnopqrztuvwxyz1234567890'.split('')
+    return [0,0,0,0].map(code => {
+      return char[Math.floor(Math.random() * char.length)]
+    }).join('')
+  }
+
+
+  _createGame() {
+    const code = this._genAccessCode()
+    // call backend
+    return <h2>{`Access Code: ${code}`}</h2>
+  }
+
+  _joinGame() {
+    return (
+      <form >
+        <div className="form-group">
+          <input type="text" value={code} placeholder='Enter Access Code' onChange={this._handleChange}/>
+        </div>
+      </form>
+    )
+  }
+
+
+
+  _lobby() {
+    return (
+      <button type="button" class="btn btn-default" onClick={this._displayState(true, false)}>Join Game</button>
+      <button type="button" class="btn btn-default" onClick={this._displayState(false, true)}>Create Game</button>
+    )
+  }
+
+  _displayState(join, create) {
+    this.setState({
+          inLobby: false,
+          join: join,
+          create: create
+        })
+  }
+
+  _displayLogic() {
+    const {inLobby, join, create} = this.state
+    if(inLobby) return this._lobby()
+    if(join) return this._joinGame()
+    if(create) return this._createGame()
   }
 
   render() {
     return (
-      <div></div>
+      <div>
+        {this._displayLogic()}
+      </div>
     )
   }
 }
