84e32eee7e0cb5a0ddc29077d6d19abb07c1701b
Christopher Wiles <chriswiles87@gmail.com>
2016-09-05 10:28:14 -0500

    display GameLobby or GameBoard

--- client/components/App.js
+++ client/components/App.js
@@ -10,14 +10,20 @@ export default class App extends Component {
     }
   }
 
+  _setLobby() {
+    this.setState({isLobby: !this.state.isLobby})
+  }
+
   render() {
     return (
       <div className="container">
           <div className="row">
               <div className="col-md-offset-2 col-md-8">
-                <div className='title'>Gomoku</div>
                 {/* <div className='subTitle'>Tic-Tac-Toe get 5 in a row</div> */}
-                <div className="gameBoard"><GameBoard/></div>
+                <div className="gameBoard">
+                  <div className='title'>Gomoku</div>
+                  {this.state.isLobby ? <GameLobby setLobby={this._setLobby.bind(this)}/> : <GameBoard setLobby={this._setLobby.bind(this)}/>}
+                </div>
               </div>
           </div>
       </div>
