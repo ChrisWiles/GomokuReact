a10fe09effb914b1368cd32079612a8352a28353
Christopher Wiles <chriswiles87@gmail.com>
2016-11-08 13:20:24 -0600

    convert to functional stateless

--- client/components/GameTile.js
+++ client/components/GameTile.js
@@ -1,38 +1,24 @@
 import React, {Component, PropTypes} from 'react'
 
-class GameTile extends Component {
-  constructor(props) {
-    super(props)
-    // bind your event handlers in the constructor so they are only bound once for every instance
-    this._handleOnClick = this._handleOnClick.bind(this)
-  }
+const GameTile = ({isTurn, updatePlayerMove, id, player}) => {
 
-  _handleOnClick() {
-    const {isTurn, updatePlayerMove, id} = this.props
-    if (isTurn) {
-      updatePlayerMove(id)
-    }
-  }
+  const handleOnClick = () => isTurn && updatePlayerMove(id)
 
-  _tile(player) {
-    if (player === 1) {
-      return <a href="#" className="btn btn-primary btn-circle"/>
-    } else if (player === 2) {
-      return <a href="#" className="btn btn-success btn-circle"/>
-    } else if (player === 'win') {
-      return <a href="#" className="btn btn-danger btn-circle"/>
-    } else {
-      return <a href="#" className="btn btn-default btn-circle" onClick={this._handleOnClick}/>
-    }
-  }
-
-  render() {
-    return this._tile(this.props.player)
+  if (player === 1) {
+    return <a href="#" className="btn btn-primary btn-circle"/>
+  } else if (player === 2) {
+    return <a href="#" className="btn btn-success btn-circle"/>
+  } else if (player === 'win') {
+    return <a href="#" className="btn btn-danger btn-circle"/>
+  } else {
+    return <a href="#" className="btn btn-default btn-circle" onClick={handleOnClick}/>
   }
 }
 
 GameTile.propTypes = {
   isTurn: PropTypes.bool.isRequired,
+  updatePlayerMove: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired
 }
 
 export default GameTile
